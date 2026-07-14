0a8aa717eed465a6797b67b96090e5d2b397b019
sandhiya2203 <107338809+sandhiya2203@users.noreply.github.com>
2026-07-14 11:56:34 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
+import os
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+JSON_FILE = "/home/sandiya/Downloads/sdclib-master/patient_data.json"
+
+
+@app.get("/")
+def home():
+    return {
+        "message":"SDC Patient REST API Running"
+    }
+
+
+@app.get("/patient")
+def patient():
+
+    print("Looking for:", JSON_FILE)
+
+
+    if not os.path.exists(JSON_FILE):
+
+        print("JSON NOT FOUND")
+
+        return {
+            "status":"Disconnected",
+            "heart_rate":0,
+            "blood_pressure":"0/0",
+            "spo2":"0"
+        }
+
+
+    print("JSON FOUND")
+
+
+    with open(JSON_FILE,"r") as f:
+
+        data = json.load(f)
+
+    return data
