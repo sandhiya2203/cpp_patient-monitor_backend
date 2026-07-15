7ee7e6684c55e4734509469d9780e142920a37ce
sandhiya2203 <107338809+sandhiya2203@users.noreply.github.com>
2026-07-15 14:08:23 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -14,46 +12,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 patient_data = {
 
-    "status": "Disconnected",
-    "heartRate": 0,
-    "bloodPressure": "0/0",
-    "spo2": 0
-
+    "status":"Disconnected",
+    "heartRate":0,
+    "bloodPressure":"0/0",
+    "spo2":0
 }
 
 
 
 @app.post("/update")
-def update(data: dict):
+def update(data:dict):
 
-    print("DATA RECEIVED FROM C++:")
+    print("DATA RECEIVED:")
     print(data)
 
-    patient_data["status"] = data.get(
-        "status",
-        patient_data["status"]
-    )
-
-    patient_data["heartRate"] = data.get(
-        "heartRate",
-        patient_data["heartRate"]
-    )
-
-    patient_data["bloodPressure"] = data.get(
-        "bloodPressure",
-        patient_data["bloodPressure"]
-    )
-
-    patient_data["spo2"] = data.get(
-        "spo2",
-        patient_data["spo2"]
-    )
-
+    patient_data.update(data)
 
     return {
-        "message":"updated",
+        "status":"updated",
         "data":patient_data
     }
 
@@ -63,20 +42,3 @@ def update(data: dict):
 def patient():
 
     return patient_data
-
-
-
-@app.get("/")
-def home():
-
-    return FileResponse(
-        "static/index.html"
-    )
-
-
-
-app.mount(
-    "/static",
-    StaticFiles(directory="static"),
-    name="static"
-)
